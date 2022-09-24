ac87b4b29fcbd9853c1e1e031af71e36db87ed82
ccd729 <funbaba@live.com>
2022-09-24 01:53:31 -0700

    Created simple white circle brush following cursor

--- PaintingTool.cpp
+++ PaintingTool.cpp
@@ -6,23 +6,37 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 
+//Brush default properties
+const int BrushRadiusDefault = 15;
+const sf::Color BrushColorDefault = sf::Color::White;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    //Variables for brush
+    int brushRadius = BrushRadiusDefault;
+    sf::Color brushColor = BrushColorDefault;
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //Create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My Little Drawing Tool");
 
+    // Create brush
+    sf::CircleShape shape(brushRadius);
+    shape.setFillColor(brushColor);
+
+    // Game loop
     while (window.isOpen())
     {
+        //Window close handler
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        window.clear();
+        //window.clear();
+        
+        // Set brush position to the mouse cursor
+        shape.setPosition(sf::Mouse::getPosition(window).x - brushRadius, sf::Mouse::getPosition(window).y - brushRadius);
         window.draw(shape);
         window.display();
     }
