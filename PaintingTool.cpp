38ae85d6c28834e96c20f1dc4c8551ebb12d6b28
ccd729 <funbaba@live.com>
2022-09-24 19:03:56 -0700

    Added painting stack with undo and save file function

--- PaintingTool.cpp
+++ PaintingTool.cpp
@@ -6,23 +6,35 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 #include <iostream>
+#include <vector>
 
 //Brush default properties
-const int BrushRadiusDefault = 15;
+const float BrushRadiusDefault = 15.f;
 const sf::Color BrushColorDefault = sf::Color(170, 196, 255);
-
 int main()
 {
     //Variables for brush
-    int brushRadius = BrushRadiusDefault;
+    float brushRadius = BrushRadiusDefault;
     sf::Color brushColor = BrushColorDefault;
 
     //Create the window
+    //sf::ContextSettings settings;
+    //settings.antialiasingLevel = 8;
+    //sf::RenderWindow window(sf::VideoMode(800, 600), "My Little Drawing Tool", sf::Style::Default, settings);
     sf::RenderWindow window(sf::VideoMode(800, 600), "My Little Drawing Tool");
+    window.setFramerateLimit(60);
 
     // Create brush
     sf::CircleShape shape(brushRadius);
     shape.setFillColor(brushColor);
+    //Create stack for drawing steps
+    std::vector<std::vector<sf::Shape*>> shapeStack;
+    std::vector<sf::Shape*> currentDraw;
+
+    //Boolean variables
+    bool saveRelease = true;
+    bool undoRelease = true;
+    bool mouseLeftDown = false;
 
     // Game loop
     while (window.isOpen())
@@ -33,28 +45,68 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            // Space key creates a screenshot
-            if (event.key.code == sf::Keyboard::Space) {
-                sf::Texture texture;
-                texture.create(window.getSize().x, window.getSize().y);
-                texture.update(window);
-                if (texture.copyToImage().saveToFile("Mywork.png"))
+            //Save current draw trail
+            else if (event.type == sf::Event::MouseMoved && mouseLeftDown){
+                if (mouseLeftDown) {
+                    shape.setFillColor(brushColor);
+                }
+                shape.setPosition(sf::Mouse::getPosition(window).x - brushRadius, sf::Mouse::getPosition(window).y - brushRadius);
+                currentDraw.push_back(new sf::CircleShape(shape));
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.key.code == sf::Mouse::Left) {
+                    mouseLeftDown = true;
+                    shape.setPosition(sf::Mouse::getPosition(window).x - brushRadius, sf::Mouse::getPosition(window).y - brushRadius);
+                    currentDraw.push_back(new sf::CircleShape(shape));
+                }
+            }
+            else if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.key.code == sf::Mouse::Left) {
+                    mouseLeftDown = false;
+                }
+                shapeStack.push_back(currentDraw);
+                currentDraw.clear();
+            }
+            //save & undo boolean reset
+            else if (event.type == sf::Event::KeyReleased) {
+                if (event.key.code == sf::Keyboard::S)
                 {
-                    std::cout << "Screenshot saved to Mywork.png" << std::endl;
+                    saveRelease = true;
+                }
+                else if (event.key.code == sf::Keyboard::Z) {
+                    undoRelease = true;
                 }
             }
         }
-        //window.clear();
-        
-        // Set brush position to the mouse cursor
-        shape.setPosition(sf::Mouse::getPosition(window).x - brushRadius, sf::Mouse::getPosition(window).y - brushRadius);
-        
-        //Only draw when the mouse left button is down
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            window.draw(shape);
+        // Ctrl+S saves a screenshot
+        if (saveRelease && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            saveRelease = false;
+            sf::Texture texture;
+            texture.create(window.getSize().x, window.getSize().y);
+            texture.update(window);
+            if (texture.copyToImage().saveToFile("Mywork.png")) {
+                std::cout << "Screenshot saved to Mywork.png" << std::endl;
+            }
+        }
+        // Ctrl+Z undos a previous step
+        if (undoRelease && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+            undoRelease = false;
+            if (!shapeStack.empty()) {
+                shapeStack.pop_back();
+            }
+        }
+        window.clear(sf::Color::Transparent);
+        // Draw completed shapes
+        for (unsigned int i = 0; i < shapeStack.size(); i++) {
+            for(unsigned int j = 0; j<shapeStack[i].size(); j++){
+                window.draw(*(shapeStack[i][j]));
+            }
+        }
+        //temp draw currentDraw
+        for (unsigned int i = 0; i < currentDraw.size(); i++) {
+            window.draw(*currentDraw[i]);
         }
         window.display();
     }
-
     return 0;
 }
